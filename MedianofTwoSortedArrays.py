5f3704a1eb6ba3bab17478e87b5f2d58e92aa851
Andrey Kapustin <andythefox@outlook.com>
2023-06-29 11:01:47 +0300

    task linked

--- MedianofTwoSortedArrays.py
+++ MedianofTwoSortedArrays.py
@@ -1,3 +1,4 @@
+# Task: https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums1 += nums2
