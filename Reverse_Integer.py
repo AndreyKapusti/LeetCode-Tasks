6578fee73ae6009686ff9a773a1a98b307b5ce01
Andrey Kapustin <andythefox@outlook.com>
2023-08-13 21:05:34 +0300

    Reverse integer

---
+++ Reverse_Integer.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x in range(-9, 10):
+            return x
+        r=int((str(abs(x))[::-1]).lstrip('0'))*(-1 if x < 0 else 1)
+        return r if -2147483648 <= r <= 2147483647 else 0
